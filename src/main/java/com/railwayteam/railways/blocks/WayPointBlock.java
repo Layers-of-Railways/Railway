c06ef0430aa612fa27920a604b77ffb653feece2
LittleCircles <balzoe831@hotmail.com>
2020-07-22 09:56:15 -0500

    Added: .hardnessAndResistance(5.0f, 6.0f) //Can redo later 				//Add more Later
    Mainly a Test to see if this works how I think

--- src/main/java/com/railwayteam/railways/blocks/WayPointBlock.java
+++ src/main/java/com/railwayteam/railways/blocks/WayPointBlock.java
@@ -8,17 +8,25 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.Direction;
+import net.minecraft.util.datafix.fixes.EntityItemFrameFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
+import static net.minecraft.block.WallSkullBlock.FACING;
+
+
 public class WayPointBlock extends Block{
 	public static final String name = "waypoint";
-	
+
 	public WayPointBlock() {
-		super(Properties.from(Blocks.OAK_FENCE));
+		super(Properties.from(Blocks.OAK_FENCE)
+				.hardnessAndResistance(5.0f, 6.0f) //Can redo later
+				//Add more Later
+		);
 		setRegistryName(Railways.createResourceLocation(name));
 	}
 	
@@ -38,9 +46,12 @@ public class WayPointBlock extends Block{
     public BlockRenderType getRenderType(@Nonnull BlockState p_149645_1_) {
         return BlockRenderType.MODEL;
     }
-	
+
 	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
 		return VoxelShapes.or(VoxelShapes.create(7.0f/16, 0, 7.0f/16, 9.0f/16, 12.0f/16, 9.0f/16), VoxelShapes.create(7.5f/16, 6.5f/16, 0, 8.5f/16, 11.5f/16, 7f/16));
+
+
+		
 	}
 }
