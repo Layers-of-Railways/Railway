136faf06804e0d58e37521a1679472c3af6d07a0
talrey <cycleman.atl@gmail.com>
2020-08-09 01:19:29 -0700

    waypoint tool can now place rails from inventory between valid connections (with varying Y along line)

--- src/main/java/com/railwayteam/railways/items/WayPointToolItem.java
+++ src/main/java/com/railwayteam/railways/items/WayPointToolItem.java
@@ -1,16 +1,28 @@
 package com.railwayteam.railways.items;
 
+import com.railwayteam.railways.ModSetup;
 import com.railwayteam.railways.blocks.WayPointBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RailBlock;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.NBTUtil;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.*;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.eventbus.api.Event;
+
+import static net.minecraftforge.eventbus.api.Event.Result.DEFAULT;
+import static net.minecraftforge.eventbus.api.Event.Result.DENY;
 
 public class WayPointToolItem extends Item{
 	public static final String name      = "waypoint_manager";
@@ -62,6 +74,7 @@ public class WayPointToolItem extends Item{
 						context.getItem().setTag(null);
 						// this is where we'd fire the event to build a track connection...
 						// TODO: fire track path connection event stuff
+						tryToPlaceTracks(player, first, pos);
 						return ActionResultType.SUCCESS;
 					}
 					else {
@@ -73,5 +86,80 @@ public class WayPointToolItem extends Item{
 		}
 		// else player is null
 		return super.onItemUse(context);
+	}
+
+	private boolean tryToPlaceTracks (PlayerEntity player, BlockPos start, BlockPos end) {
+		if (player.world.isRemote || start.equals(end)) return false;
+
+		// let's figure out the direction to iterate
+		Vec3i delta = end.subtract(start);
+		int stepX = delta.getX()==0 ? 0 : Math.abs(delta.getX())/delta.getX();
+		int stepZ = delta.getZ()==0 ? 0 : Math.abs(delta.getZ())/delta.getZ();
+		int slot  = 0;
+		ItemStack stack = null;
+		BlockPos step = new BlockPos(start.getX(), start.getY(), start.getZ());
+
+		// now it's time to iterate
+		while (!step.equals(end)) {
+			// first, check if we're making a valid path
+			boolean valid = false;
+
+			if (player.world.getBlockState(step).getBlock() instanceof WayPointBlock) {
+				valid = true;
+				player.world.addEntity(
+				  new ItemEntity(player.world, step.getX(), step.getY(), step.getZ(),
+					  new ItemStack(ModSetup.R_BLOCK_WAYPOINT.get(), 1))
+				);
+			}
+			// logic:
+			// if next block is air:
+			//   start checking down. if next down block is air:
+			//     if next down-2 block is NOT air:
+			//       valid.
+			// else check up:
+			//   if next up block is air:
+			//     valid
+			else if (player.world.isAirBlock(step)) {
+				if (player.world.isAirBlock(step.add(0,-1,0))) {
+					if (!player.world.isAirBlock(step.add(0,-2,0))) {
+						valid = true;
+						step = step.add(0,-1,0);
+					}
+				}
+				else valid = true;
+			}
+			else if (player.world.isAirBlock(step.add(0,1,0))) {
+				valid = true;
+				step = step.add(0,1,0);
+			}
+			// validation done, get a rail from the player's inventory
+			if (valid) {
+				if (stack == null || stack.isEmpty()) {
+					while (slot < player.inventory.getSizeInventory()) {
+						ItemStack check = player.inventory.getStackInSlot(slot);
+						if (ItemTags.getCollection().getOrCreate(ItemTags.RAILS.getId()).contains(check.getItem())) {
+							stack = check;
+							break;
+						}
+						slot++;
+					}
+				}
+				// we got a rail, let's place it
+				if (stack.getCount() > 0) {
+					//	player.sendMessage(new StringTextComponent("DEBUG: placed at " + step.toString()));
+					player.world.setBlockState(step, Blocks.RAIL.getDefaultState(), Constants.BlockFlags.BLOCK_UPDATE);
+					stack.setCount(stack.getCount() - 1);
+				}
+			}
+			// escape if we can't get any more
+			if (slot == player.inventory.getSizeInventory() && stack.isEmpty()) break;
+
+			// otherwise step to the next iteration
+			if (stepX==0) {
+				step = step.add(0,0,stepZ);
+			}
+			else step = step.add(stepX,0,0);
+		}
+		return step.equals(end); // false if we failed early for whatever reason
 	}
 }
