b5d32c01bb180616db193be6f83ea68a9e908901
talrey <cycleman.atl@gmail.com>
2021-04-22 20:22:45 -0700

    affected by mappings changes during Create integration

--- src/main/java/com/railwayteam/railways/RailPathPlanRenderer.java
+++ src/main/java/com/railwayteam/railways/RailPathPlanRenderer.java
@@ -41,10 +41,10 @@ public class RailPathPlanRenderer {
     ItemStack flag = viewer.getHeldItem(Hand.MAIN_HAND);
 
     MatrixStack ms       = event.getMatrix();
-    Matrix4f m4f         = ms.getLast().getMatrix();
+    Matrix4f m4f         = ms.peek().getModel(); //ms.getLast().getMatrix();
     VoxelShape vs        = world.getBlockState(eventPos).getShape(world, eventPos);
     ActiveRenderInfo ari = event.getInfo();
-    IVertexBuilder vb    = event.getBuffers().getBuffer(RenderType.LINES);
+    IVertexBuilder vb    = event.getBuffers().getBuffer(RenderType.getLines());
     double eyeX = ari.getProjectedView().getX();
     double eyeY = ari.getProjectedView().getY();
     double eyeZ = ari.getProjectedView().getZ();
@@ -58,17 +58,17 @@ public class RailPathPlanRenderer {
       float x0 = 0.5f;
       float y0 = 0.5f;
       float z0 = 0.5f;
-        vb.pos(m4f, (float) (x0 + pos1.getX() - eyeX), (float) (y0 + pos1.getY() - eyeY), (float) (z0 + pos1.getZ() - eyeZ))
+        vb.vertex(m4f, (float) (x0 + pos1.getX() - eyeX), (float) (y0 + pos1.getY() - eyeY), (float) (z0 + pos1.getZ() - eyeZ))
         .color(0f, 1f, 0f, 1f).endVertex();
-        vb.pos(m4f, (float) (x0 + pos2.getX() - eyeX), (float) (y0 + pos2.getY() - eyeY), (float) (z0 + pos2.getZ() - eyeZ))
+        vb.vertex(m4f, (float) (x0 + pos2.getX() - eyeX), (float) (y0 + pos2.getY() - eyeY), (float) (z0 + pos2.getZ() - eyeZ))
         .color(0f, 1f, 0f, 1f).endVertex();
       //});
     }
     else {
       vs.forEachEdge((x0, y0, z0, x1, y1, z1) -> {
-        vb.pos(m4f, (float) (x0 + eventPos.getX() - eyeX), (float) (y0 + eventPos.getY() - eyeY), (float) (z0 + eventPos.getZ() - eyeZ))
+        vb.vertex(m4f, (float) (x0 + eventPos.getX() - eyeX), (float) (y0 + eventPos.getY() - eyeY), (float) (z0 + eventPos.getZ() - eyeZ))
         .color(0f, 1f, 0f, 0.5f).endVertex();
-        vb.pos(m4f, (float) (x1 + eventPos.getX() - eyeX), (float) (y1 + eventPos.getY() - eyeY), (float) (z1 + eventPos.getZ() - eyeZ))
+        vb.vertex(m4f, (float) (x1 + eventPos.getX() - eyeX), (float) (y1 + eventPos.getY() - eyeY), (float) (z1 + eventPos.getZ() - eyeZ))
         .color(0f, 1f, 0f, 0.5f).endVertex();
       });
     }
