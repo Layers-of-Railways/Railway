6fe7a3f03276b2bc3a94637713916c464993eff2
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-18 13:29:08 +0300

    update to create 0.3.2

---
+++ src/main/java/com/railwayteam/railways/registry/CRPartials.java
@@ -0,0 +1,26 @@
+package com.railwayteam.railways.registry;
+
+import com.jozufozu.flywheel.core.PartialModel;
+import com.railwayteam.railways.Railways;
+import com.simibubi.create.Create;
+import net.minecraft.util.ResourceLocation;
+
+public class CRPartials {
+
+
+    public static PartialModel get(String name) {
+        return new PartialModel(new ResourceLocation(Railways.MODID, "block/" + name));
+    }
+
+    public static PartialModel getCreate(String name) {
+        return new PartialModel(Create.asResource("block/" + name));
+    }
+
+    public static PartialModel getEntity(String name) {
+        return new PartialModel(new ResourceLocation(Railways.MODID, "entity/" + name));
+    }
+
+    public static PartialModel getEntityCreate(String name) {
+        return new PartialModel(Create.asResource("entity/" + name));
+    }
+}
