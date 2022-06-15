34e04d4c27208a814fff353666a0b65ca2a6d5af
talrey <cycleman.atl@gmail.com>
2022-06-14 23:49:25 -0700

    lighting the stove

--- src/main/java/com/railwayteam/railways/content/Steamcart/SteamCartEntity.java
+++ src/main/java/com/railwayteam/railways/content/Steamcart/SteamCartEntity.java
@@ -6,18 +6,31 @@ import com.railwayteam.railways.registry.CRItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.ForgeHooks;
 
 public class SteamCartEntity extends MinecartBlock {
   private boolean powered;
   private int toggleCooldown, fuel, water;
   private static final int COOLDOWN = 100; // ticks
+  private static final float BURN_RATIO = 3600f / ForgeHooks.getBurnTime(new ItemStack(Items.COAL), RecipeType.SMELTING); // 3600 ticks is from the Furnace Minecart
 
   protected Vec3 directionCache;
+
+  public double xPush;
+  public double zPush;
 
   public SteamCartEntity (EntityType<?> type, Level level) {
     super(type, level, CRBlocks.BLOCK_STEAMCART.get());
@@ -26,6 +39,8 @@ public class SteamCartEntity extends MinecartBlock {
     directionCache = Vec3.ZERO;
     fuel = 0;
     water = 0;
+    xPush = 0d;
+    zPush = 0d;
   }
 
   public boolean isPowered () {
@@ -48,26 +63,93 @@ public class SteamCartEntity extends MinecartBlock {
   }
 
   @Override
+  public InteractionResult interact (Player player, InteractionHand hand) {
+    InteractionResult ret = super.interact(player, hand);
+    if (ret.consumesAction()) return ret;
+
+    ItemStack stack = player.getItemInHand(hand);
+    if (ItemTags.COALS.contains(stack.getItem()) && fuel <= 32000) {
+      fuel += Math.round(ForgeHooks.getBurnTime(stack, RecipeType.SMELTING) * BURN_RATIO);
+      if (!player.getAbilities().instabuild) stack.shrink(1);
+    }
+    else if (stack.getItem().equals(Items.WATER_BUCKET) && water <= 32000) {
+      water += 3600;
+      if (!player.getAbilities().instabuild) player.setItemInHand(hand, new ItemStack(Items.BUCKET));
+      player.awardStat(Stats.ITEM_USED.get(Items.WATER_BUCKET));
+    }
+
+    if (fuel > 0 && water > 0) {
+      this.xPush = this.getX() - player.getX();
+      this.zPush = this.getZ() - player.getZ();
+      setCartRunning(true);
+    }
+    return InteractionResult.sidedSuccess(this.level.isClientSide);
+  }
+
+  @Override
   public void tick () {
     super.tick();
     if (toggleCooldown > 0) toggleCooldown--;
 
-    if (this.powered) {
+    if (!powered) {
+      this.xPush = 0.0D;
+      this.zPush = 0.0D;
+    }
+    else {
+      if (water > fuel) {
+        --water;
+      }
+      else if (fuel > 0) {
+        --fuel;
+      }
+      else { // fuel and water are <= 0
+        setCartRunning(false);
+      }
+
       if (this.random.nextInt(4) == 0) {
-        this.level.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, this.getX(), this.getY() + 0.8D, this.getZ(), 0.0D, 0.0D, 0.0D);
+        this.level.addParticle(ParticleTypes.CLOUD, this.getX(), this.getY() + 0.8D, this.getZ(), 0.0D, 0.0D, 0.0D);
       }
     }
   }
 
   @Override
   protected void moveAlongTrack(BlockPos pos, BlockState state) {
-    super.moveAlongTrack(pos, state);
     directionCache = this.getDeltaMovement();
+
+    // taken from the Furnace Minecart
+    double d0 = 1.0E-4D;
+    double d1 = 0.001D;
+    super.moveAlongTrack(pos, state);
+    Vec3 vec3 = this.getDeltaMovement();
+    double d2 = vec3.horizontalDistanceSqr();
+    double d3 = this.xPush * this.xPush + this.zPush * this.zPush;
+    if (d3 > 1.0E-4D && d2 > 0.001D) {
+      double d4 = Math.sqrt(d2);
+      double d5 = Math.sqrt(d3);
+      this.xPush = vec3.x / d4 * d5;
+      this.zPush = vec3.z / d4 * d5;
+    }
   }
 
   @Override
   protected void applyNaturalSlowdown() {
-    if (!powered) super.applyNaturalSlowdown();
+    // taken from the Furnace Minecart
+    double d0 = this.xPush * this.xPush + this.zPush * this.zPush;
+    if (d0 > 1.0E-7D) {
+      d0 = Math.sqrt(d0);
+      this.xPush /= d0;
+      this.zPush /= d0;
+      Vec3 vec3 = this.getDeltaMovement().multiply(0.8D, 0.0D, 0.8D).add(this.xPush, 0.0D, this.zPush);
+      if (this.isInWater()) {
+        vec3 = vec3.scale(0.1D);
+      }
+
+      this.setDeltaMovement(vec3);
+    } else {
+      this.setDeltaMovement(this.getDeltaMovement().multiply(0.98D, 0.0D, 0.98D));
+    }
+
+    super.applyNaturalSlowdown();
   }
 
   @Override
@@ -75,6 +157,17 @@ public class SteamCartEntity extends MinecartBlock {
     if ((toggleCooldown <= 0) && active) {
       toggleCartRunning();
     }
+  }
+
+  // values taken from the Furnace Minecart
+  @Override
+  protected double getMaxSpeed() {
+    return (this.isInWater() ? 3.0D : 4.0D) / 20.0D;
+  }
+
+  @Override
+  public float getMaxCartSpeedOnRail() {
+    return 0.2f;
   }
 
   @Override
@@ -89,7 +182,11 @@ public class SteamCartEntity extends MinecartBlock {
   }
 
   public void toggleCartRunning () {
-    powered = !powered;
+    setCartRunning(!powered);
+  }
+
+  protected void setCartRunning(boolean power) {
+    powered = power;
     toggleCooldown = COOLDOWN;
     if (!level.isClientSide()) {
     //  Railways.LOGGER.error("toggled cart state");
@@ -100,5 +197,23 @@ public class SteamCartEntity extends MinecartBlock {
   public BlockState getDisplayBlockState () {
     boolean negative = getMotionDirection().getAxisDirection() == Direction.AxisDirection.NEGATIVE;
     return content.setValue(SteamCartBlock.POWERED, isPowered()).setValue(SteamCartBlock.FACING, negative ? Direction.WEST : Direction.EAST);
+  }
+
+  protected void addAdditionalSaveData(CompoundTag tag) {
+    super.addAdditionalSaveData(tag);
+    tag.putDouble("PushX", this.xPush);
+    tag.putDouble("PushZ", this.zPush);
+    tag.putInt("Fuel", this.fuel);
+    tag.putInt("Water", this.water);
+    tag.putBoolean("Powered", this.powered);
+  }
+
+  protected void readAdditionalSaveData(CompoundTag tag) {
+    super.readAdditionalSaveData(tag);
+    this.xPush   = tag.getDouble("PushX");
+    this.zPush   = tag.getDouble("PushZ");
+    this.fuel    = tag.getInt("Fuel");
+    this.water   = tag.getInt("Water");
+    this.powered = tag.getBoolean("Powered");
   }
 }
