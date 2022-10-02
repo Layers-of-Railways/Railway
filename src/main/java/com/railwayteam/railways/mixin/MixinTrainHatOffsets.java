29ccf030de7b31cd2b7d51716e94b1fbe6ad3811
techno_sam <linux.techno.sam@gmail.com>
2022-10-02 12:52:18 -0700

    Conductor wears cap correctly when driving train

---
+++ src/main/java/com/railwayteam/railways/mixin/MixinTrainHatOffsets.java
@@ -0,0 +1,20 @@
+package com.railwayteam.railways.mixin;
+
+import com.railwayteam.railways.content.Conductor.ConductorEntityModel;
+import com.simibubi.create.content.logistics.trains.management.schedule.TrainHatOffsets;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = TrainHatOffsets.class, remap = false)
+public class MixinTrainHatOffsets {
+  @Inject(at = @At("HEAD"), method = "getOffset", cancellable = true)
+  private static void inj$getOffset(EntityModel<?> model, CallbackInfoReturnable<Vec3> cir) {
+    if (model instanceof ConductorEntityModel) {
+      cir.setReturnValue(new Vec3(0f, -1f, 0f));
+    }
+  }
+}
