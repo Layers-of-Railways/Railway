48198b74a1b776f9cc4cdcca99941447db353f4a
techno_sam <linux.techno.sam@gmail.com>
2022-11-15 17:22:51 -0800

    Fix semaphore placement server-side

--- src/main/java/com/railwayteam/railways/content/semaphore/SemaphoreItem.java
+++ src/main/java/com/railwayteam/railways/content/semaphore/SemaphoreItem.java
@@ -2,16 +2,17 @@ package com.railwayteam.railways.content.semaphore;
 
 import com.simibubi.create.foundation.utility.placement.IPlacementHelper;
 import com.simibubi.create.foundation.utility.placement.PlacementHelpers;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class SemaphoreItem extends BlockItem {
     public SemaphoreItem(Block pBlock, Properties pProperties) {
@@ -32,8 +33,12 @@ public class SemaphoreItem extends BlockItem {
             pos = pos.offset(pContext.getClickedFace().getOpposite().getNormal());
         }
         BlockState state = world.getBlockState(pos);
-        //BlockHitResult ray = pContext.getLevel().clip(new ClipContext(player.getEyePosition(),pContext.getClickLocation(), ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE,null));
-        BlockHitResult ray = (BlockHitResult)Minecraft.getInstance().hitResult;
+        double pHitDistance = player.getReachDistance();
+        Vec3 eyePos = player.getEyePosition();
+        Vec3 viewVec = player.getViewVector(0);
+        Vec3 endPos = eyePos.add(viewVec.x * pHitDistance, viewVec.y * pHitDistance, viewVec.z * pHitDistance);
+        BlockHitResult ray = pContext.getLevel().clip(new ClipContext(eyePos, endPos, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player));
+//        BlockHitResult ray = (BlockHitResult)Minecraft.getInstance().hitResult;
         if(!placementHelper.matchesState(state))
             return super.place(pContext);
 
