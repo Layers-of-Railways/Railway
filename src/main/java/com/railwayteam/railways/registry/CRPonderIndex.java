25611946c27e994b86a2df88712ec26641ca849e
techno_sam <linux.techno.sam@gmail.com>
2022-11-23 19:36:34 -0800

    Conductor ponder

--- src/main/java/com/railwayteam/railways/registry/CRPonderIndex.java
+++ src/main/java/com/railwayteam/railways/registry/CRPonderIndex.java
@@ -1,6 +1,7 @@
 package com.railwayteam.railways.registry;
 
 import com.railwayteam.railways.Railways;
+import com.railwayteam.railways.ponder.ConductorScenes;
 import com.railwayteam.railways.ponder.TrainScenes;
 import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 
@@ -9,6 +10,10 @@ public class CRPonderIndex {
     static final PonderRegistrationHelper HELPER = new PonderRegistrationHelper(Railways.MODID);
     public static void register() {
         HELPER.forComponents(CRBlocks.SEMAPHORE)
-                .addStoryBoard("semaphore", TrainScenes::signaling);
+            .addStoryBoard("semaphore", TrainScenes::signaling);
+        HELPER.forComponents(CRItems.ITEM_CONDUCTOR_CAP.values())
+            .addStoryBoard("conductor", ConductorScenes::constructing)
+            .addStoryBoard("conductor_redstone", ConductorScenes::redstoning)
+            .addStoryBoard("conductor", ConductorScenes::toolboxing);
     }
 }
