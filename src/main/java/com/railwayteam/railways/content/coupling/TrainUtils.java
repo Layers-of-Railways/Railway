bd59b5593c4ef2541a54cc9969a039131f4f2224
techno_sam <linux.techno.sam@gmail.com>
2022-12-21 12:42:27 -0800

    Decoupling part 2 - command refinements, decouple variable number of carriages
    TODO:
    Calculate controls data etc...
    Coupling by command
    (De)coupling through a block

--- src/main/java/com/railwayteam/railways/content/coupling/TrainUtils.java
+++ src/main/java/com/railwayteam/railways/content/coupling/TrainUtils.java
@@ -10,6 +10,7 @@ import com.simibubi.create.foundation.networking.AllPackets;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.network.PacketDistributor;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,10 +18,11 @@ public class TrainUtils {
     /**
      * Splits the train into two trains, with the last carriage of the original train being the first carriage of the new train.
      * @param train The train to split.
+     * @param numberOffEnd The number of carriages to move to the new train.
      * @return The new train.
      */
-    public static Train splitTrain(Train train) {
-        if (train.carriages.size() <= 1)
+    public static Train splitTrain(Train train, int numberOffEnd) {
+        if (train.carriages.size() <= numberOffEnd)
             return train;
         /*things needing changing:
             train.stress
@@ -36,38 +38,57 @@ public class TrainUtils {
 
             newTrain
         */
-        Carriage lastCarriage = train.carriages.remove(train.carriages.size() - 1);
+        Carriage[] lastCarriages = new Carriage[numberOffEnd];
+        Integer[] lastCarriageSpacings = new Integer[numberOffEnd - 1];
+
+        for (int i = numberOffEnd-1; i >= 0; i--) {
+            lastCarriages[i] = train.carriages.remove(train.carriages.size() - 1);
+            if (i > 0) {
+                lastCarriageSpacings[i-1] = train.carriageSpacing.remove(train.carriageSpacing.size() - 1);
+            }
+        }
+
+//        Carriage lastCarriage = train.carriages.remove(train.carriages.size() - 1);
         double[] originalStress = ((AccessorTrain) train).snr_getStress();
-        double[] newStress = new double[originalStress.length - 1];
+        double[] newStress = new double[originalStress.length - numberOffEnd];
         System.arraycopy(originalStress, 0, newStress, 0, newStress.length);
         ((AccessorTrain) train).snr_setStress(newStress);
-        train.carriageSpacing.remove(train.carriageSpacing.size() - 1);
+//        train.carriageSpacing.remove(train.carriageSpacing.size() - 1);
 
-        Train newTrain = new Train(UUID.randomUUID(), train.owner, train.graph, List.of(lastCarriage), List.of(), train.doubleEnded);
+        Train newTrain = new Train(UUID.randomUUID(), train.owner, train.graph, List.of(lastCarriages), List.of(lastCarriageSpacings), train.doubleEnded);
         newTrain.name = Component.literal("Split off from: "+train.name.getString());
 //        lastCarriage.setTrain(newTrain);
 //        lastCarriage.storage = null; //since storage is per-carriage, not per-train, this should be fine
-        lastCarriage.forEachPresentEntity(cce -> {
-            cce.carriageIndex = 0;
-            cce.trainId = newTrain.id;
-            cce.setCarriage(lastCarriage);
+        for (int i = 0; i < lastCarriages.length; i++) {
+            Carriage lastCarriage = lastCarriages[i];
+            int finalI = i;
+            lastCarriage.forEachPresentEntity(cce -> {
+                cce.carriageIndex = finalI;
+                cce.trainId = newTrain.id;
+                cce.setCarriage(lastCarriage);
 //            CarriageContraption cc = (CarriageContraption) cce.getContraption();
-            cce.syncCarriage();
-        });
+                cce.syncCarriage();
+            });
+        }
         newTrain.collectInitiallyOccupiedSignalBlocks();
         Create.RAILWAYS.addTrain(newTrain);
         AllPackets.channel.send(PacketDistributor.ALL.noArg(), new TrainPacket(newTrain, true));
 //        AllPackets.channel.send(PacketDistributor.ALL.noArg(), new TrainPacket(train, true));
 
-        lastCarriage.forEachPresentEntity(CarriageContraptionEntity::syncCarriage);
+        Arrays.stream(lastCarriages).forEach(c -> c.forEachPresentEntity(CarriageContraptionEntity::syncCarriage));
+//        lastCarriage.forEachPresentEntity(CarriageContraptionEntity::syncCarriage);
         train.carriages.forEach(carriage -> carriage.forEachPresentEntity(CarriageContraptionEntity::syncCarriage));
         newTrain.carriages.forEach(carriage -> carriage.forEachPresentEntity(CarriageContraptionEntity::syncCarriage));
 
         //TODO clientside carriages need to update carriage.train and cce.trainId
         // if we update cce.trainId and set cce.carriage to null and call cce.bindCarriage() and then
         // set cce.carriage.train to the correct train, we should be good (try skipping this last line to test some stuff)
-        lastCarriage.forEachPresentEntity(cce -> CRPackets.channel.send(PacketDistributor.ALL.noArg(), new CarriageContraptionEntityUpdatePacket(cce, newTrain)));
-        CRPackets.channel.send(PacketDistributor.ALL.noArg(), new ChopTrainEndPacket(train, 1));
+        Arrays.stream(lastCarriages).forEach(
+            c -> c.forEachPresentEntity(
+                cce -> CRPackets.channel.send(PacketDistributor.ALL.noArg(), new CarriageContraptionEntityUpdatePacket(cce, newTrain))
+            )
+        );
+        CRPackets.channel.send(PacketDistributor.ALL.noArg(), new ChopTrainEndPacket(train, numberOffEnd));
 
         return newTrain;
     }
