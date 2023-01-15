7aab38f4385958fd347abcee2317ec2083270984
techno_sam <linux.techno.sam@gmail.com>
2023-01-15 14:33:59 -0800

    Monorail blocks

--- src/main/java/com/railwayteam/railways/content/custom_tracks/monorail/MonorailBlockStateGenerator.java
+++ src/main/java/com/railwayteam/railways/content/custom_tracks/monorail/MonorailBlockStateGenerator.java
@@ -1,6 +1,9 @@
 package com.railwayteam.railways.content.custom_tracks.monorail;
 
 import com.railwayteam.railways.content.custom_tracks.CustomTrackBlockStateGenerator;
+import com.simibubi.create.Create;
+import com.simibubi.create.content.logistics.trains.track.TrackBlock;
+import com.simibubi.create.content.logistics.trains.track.TrackShape;
 import com.tterrag.registrate.providers.DataGenContext;
 import com.tterrag.registrate.providers.RegistrateBlockstateProvider;
 import net.minecraft.world.level.block.Block;
@@ -10,7 +13,13 @@ import net.minecraftforge.client.model.generators.ModelFile;
 public class MonorailBlockStateGenerator extends CustomTrackBlockStateGenerator {
     @Override
     public <T extends Block> ModelFile getModel(DataGenContext<Block, T> ctx, RegistrateBlockstateProvider prov, BlockState state) {
+        //return prov.models()
+        //    .getExistingFile(prov.modLoc("block/monorail/monorail"));
+        TrackShape value = state.getValue(TrackBlock.SHAPE);
+        if (value == TrackShape.NONE)
+            return prov.models()
+                .getExistingFile(prov.mcLoc("block/air"));
         return prov.models()
-            .getExistingFile(prov.modLoc("block/monorail/monorail"));
+            .getExistingFile(prov.modLoc("block/monorail/monorail/static_blocks/" + value.getModel()));
     }
 }
