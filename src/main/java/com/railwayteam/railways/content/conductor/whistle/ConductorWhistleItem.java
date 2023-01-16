64fadc2e58659d599a4320efa5624c7a91fef848
SpottyTheTurtle <69260662+SpottyTheTurtle@users.noreply.github.com>
2023-01-16 21:41:33 +0000

    whistle sound now actually works -now works with all rail types

--- src/main/java/com/railwayteam/railways/content/conductor/whistle/ConductorWhistleItem.java
+++ src/main/java/com/railwayteam/railways/content/conductor/whistle/ConductorWhistleItem.java
@@ -1,16 +1,23 @@
 package com.railwayteam.railways.content.conductor.whistle;
 
 import com.railwayteam.railways.registry.CRSounds;
+import com.railwayteam.railways.registry.CRTags;
 import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.logistics.trains.entity.CarriageContraptionEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import javax.annotation.Nonnull;
 
@@ -18,10 +25,10 @@ public class ConductorWhistleItem extends Item {
 
     public ConductorWhistleItem(Item.Properties properties) {
         super(properties);
-
     }
-    static boolean isTrack (Block block) { return block.equals( AllBlocks.TRACK.get()); }
-    static boolean isTrack (BlockState state) { return isTrack(state.getBlock()); }
+
+    static boolean isTrack (Block block) { return CRTags.AllBlockTags.TRACKS.matches(block); }
+    static boolean isTrack (BlockState state) { return CRTags.AllBlockTags.TRACKS.matches(state); }
     static boolean isTrack (Level level, BlockPos pos) { return isTrack(level.getBlockState(pos)); }
 
     @Nonnull
@@ -32,20 +39,31 @@ public class ConductorWhistleItem extends Item {
         Player player = ctx.getPlayer();
 
         if (isTrack(level, pos)) {
-            if (level.isClientSide)
-                ctx.getLevel().playSound(player, pos, CRSounds.CONDUCTOR_WHISTLE.get(), SoundSource.PLAYERS, 1f, 1f);
-                return InteractionResult.SUCCESS;
+            level.playSound(null, pos, CRSounds.CONDUCTOR_WHISTLE.get(),
+                    SoundSource.BLOCKS, 2f, 1f);
 
 
-
-//            level.removeBlock(pos, false);
-//            ConductorEntity.spawn(level, pos, ctx.getItemInHand().copy());
-//            if (ctx.getPlayer() != null && !ctx.getPlayer().isCreative()) {
-//                ctx.getItemInHand().shrink(1);
-//                return InteractionResult.CONSUME;
-//            }
         }
         return super.useOn(ctx);
+    }
+
+
+
+    @SubscribeEvent
+    public static void interactWithConductor(PlayerInteractEvent.EntityInteractSpecific event) {
+        Entity entity = event.getTarget();
+        Player player = event.getEntity();
+        if (player == null || entity == null)
+            return;
+        if (player.isSpectator())
+            return;
+
+        Entity rootVehicle = entity.getRootVehicle();
+        if (!(rootVehicle instanceof CarriageContraptionEntity))
+            return;
+        if (!(entity instanceof LivingEntity living))
+            return;
+
     }
 }
 
