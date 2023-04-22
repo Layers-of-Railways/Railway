eb3079e87a7b273a007d7f12d2783ea44f2cbc1b
techno-sam <linux.techno.sam@gmail.com>
2023-04-22 16:58:39 -0700

    Fix conductor cap

---
+++ common/src/main/java/com/railwayteam/railways/registry/commands/ClearCapCacheCommand.java
@@ -0,0 +1,24 @@
+package com.railwayteam.railways.registry.commands;
+
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.railwayteam.railways.content.conductor.ConductorCapModel;
+import com.railwayteam.railways.content.custom_tracks.casing.CasingRenderUtils;
+import com.railwayteam.railways.multiloader.Env;
+import com.simibubi.create.foundation.utility.Components;
+import net.minecraft.commands.SharedSuggestionProvider;
+
+import static com.railwayteam.railways.multiloader.ClientCommands.literal;
+import static com.railwayteam.railways.multiloader.ClientCommands.sendSuccess;
+
+public class ClearCapCacheCommand {
+  public static ArgumentBuilder<SharedSuggestionProvider, ?> register() {
+    return literal("clear_cap_cache")
+        .requires(cs -> cs.hasPermission(0))
+        .executes(ctx -> {
+          Env.CLIENT.runIfCurrent(() -> ConductorCapModel::clearModelCache);
+
+          sendSuccess(ctx.getSource(), Components.literal("cleared cap cache"));
+          return 1;
+        });
+  }
+}
