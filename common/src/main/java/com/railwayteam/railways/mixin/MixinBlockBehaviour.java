deaf41d5dbf62191805896528eb090eef1abee84
techno-sam <linux.techno.sam@gmail.com>
2023-06-01 15:28:49 -0700

    Tram-tastical
    drop casing slabs again when a track is broken

--- common/src/main/java/com/railwayteam/railways/mixin/MixinBlockBehaviour.java
+++ common/src/main/java/com/railwayteam/railways/mixin/MixinBlockBehaviour.java
@@ -7,6 +7,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -17,7 +19,7 @@ public class MixinBlockBehaviour {
    * @author Railways
    * @reason Can't inject in MixinTrackBlock, because TrackBlock itself doesn't include this method, only its superclass (BlockBehaviour)
    */
-//  @Inject(method = "getDrops", at = @At("RETURN"), cancellable = true)
+  @Inject(method = "getDrops", at = @At("RETURN"), cancellable = true)
   private void addTrackCasingDrops(BlockState state, LootContext.Builder builder, CallbackInfoReturnable<List<ItemStack>> cir) {
     List<ItemStack> superList = cir.getReturnValue();
     if (builder.getOptionalParameter(LootContextParams.BLOCK_ENTITY) instanceof IHasTrackCasing casing && casing.getTrackCasing() != null) {
