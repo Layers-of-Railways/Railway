0e11d73d555616085db4ffda775cfa66d294957c
SpottyTheTurtle <69260662+SpottyTheTurtle@users.noreply.github.com>
2023-06-20 13:58:46 +0100

    undo push to wrong branch

--- common/src/main/java/com/railwayteam/railways/registry/CRBogeyStyles.java
+++ common/src/main/java/com/railwayteam/railways/registry/CRBogeyStyles.java
@@ -3,7 +3,6 @@ package com.railwayteam.railways.registry;
 import com.railwayteam.railways.Railways;
 import com.railwayteam.railways.content.custom_bogeys.invisible.InvisibleBogeyRenderer;
 import com.railwayteam.railways.content.custom_bogeys.monobogey.MonoBogeyRenderer;
-import com.railwayteam.railways.content.custom_bogeys.singleaxle.SingleaxleBogeyRenderer;
 import com.simibubi.create.AllBogeyStyles;
 import com.simibubi.create.Create;
 import com.simibubi.create.content.trains.CubeParticleData;
@@ -24,12 +23,6 @@ public class CRBogeyStyles {
     public static final BogeyStyle INVISIBLE = create("invisible", Create.asResource(STANDARD_CYCLE_GROUP))
             .displayName(Components.translatable("railways.bogeys.styles.invisible"))
             .size(BogeySizes.SMALL, () -> () -> new InvisibleBogeyRenderer(), CRBlocks.INVISIBLE_BOGEY)
-            .contactParticle(new CubeParticleData())
-            .build();
-
-    public static final BogeyStyle SINGLEAXLE = create("singleaxle", Create.asResource(STANDARD_CYCLE_GROUP))
-            .displayName(Components.translatable("railways.bogeys.styles.singleaxle"))
-            .size(BogeySizes.SMALL, () -> () -> new SingleaxleBogeyRenderer(), CRBlocks.SINGLEAXLE_BOGEY)
             .contactParticle(new CubeParticleData())
             .build();
 
