f3fd1d20e71556d3d8c1779634ea0b06a12d629f
SpottyTheTurtle <69260662+SpottyTheTurtle@users.noreply.github.com>
2023-07-12 20:20:25 +0100

    Whistling away - Conductor whistles can now be automated - if a deployer clicks a station with a whistle while a train is there the whistle will be bound to the driver of the train - if there is no train present the bound train will be summoned - still has a NPE that needs fixing and more testing is required but other than that it's mostly working

--- common/src/main/java/com/railwayteam/railways/mixin/MixinStationBlock.java
+++ common/src/main/java/com/railwayteam/railways/mixin/MixinStationBlock.java
@@ -1,15 +1,32 @@
 package com.railwayteam.railways.mixin;
 
+import com.railwayteam.railways.Config;
+import com.railwayteam.railways.content.conductor.ConductorEntity;
+import com.railwayteam.railways.mixin_interfaces.ICarriageConductors;
+import com.railwayteam.railways.registry.CRBlocks;
+import com.railwayteam.railways.registry.CREntities;
+import com.simibubi.create.Create;
 import com.simibubi.create.content.kinetics.deployer.DeployerFakePlayer;
+import com.simibubi.create.content.trains.entity.Carriage;
 import com.simibubi.create.content.trains.entity.Train;
+import com.simibubi.create.content.trains.schedule.Schedule;
+import com.simibubi.create.content.trains.schedule.ScheduleEntry;
+import com.simibubi.create.content.trains.schedule.condition.ScheduledDelay;
+import com.simibubi.create.content.trains.schedule.destination.DestinationInstruction;
 import com.simibubi.create.content.trains.station.GlobalStation;
 import com.simibubi.create.content.trains.station.StationBlock;
 import com.simibubi.create.content.trains.station.StationBlockEntity;
 import com.simibubi.create.foundation.utility.VecHelper;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -22,8 +39,145 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @Mixin(value = StationBlock.class, remap = false)
 public abstract class MixinStationBlock {
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true, remap = true)
+    private void autoWhistle(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit, CallbackInfoReturnable<InteractionResult> cir){
+        ItemStack itemInHand = pPlayer.getItemInHand(pHand);
+        if (CRBlocks.CONDUCTOR_WHISTLE_FLAG.asStack().getItem().equals(itemInHand.getItem())) {
+            if (!pLevel.isClientSide && pPlayer instanceof DeployerFakePlayer && pLevel.getBlockEntity(pPos) instanceof StationBlockEntity stationBe) {
+                cir.setReturnValue(InteractionResult.CONSUME);
+                GlobalStation station = stationBe.getStation();
+                if (station != null && station.getPresentTrain() == null) {
+                    System.out.println("TOMATO");
+                    CompoundTag stackTag = itemInHand.getTag();
+                    if (stackTag == null || !stackTag.hasUUID("SelectedTrain") || !stackTag.hasUUID("SelectedConductor"))
+                        cir.setReturnValue(InteractionResult.FAIL);
+
+                    UUID trainId = stackTag.getUUID("SelectedTrain");
+                    UUID conductorId = stackTag.getUUID("SelectedConductor");
+                    BlockPos pos = stationBe.edgePoint.getPos();
+                    Level level = pPlayer.getLevel();
+
+                    if (!Create.RAILWAYS.trains.containsKey(trainId))
+                        cir.setReturnValue(InteractionResult.FAIL);
+
+                    Train train = Create.RAILWAYS.trains.get(trainId);
+                    if(train==null) cir.setReturnValue(InteractionResult.FAIL);
+                    boolean foundConductor = false;
+                    Carriage conductorCarriage = null;
+                    for (Carriage carriage : train.carriages) {
+                        if (((ICarriageConductors) carriage).getControllingConductors().contains(conductorId)) {
+                            foundConductor = true;
+                            conductorCarriage = carriage;
+                            break;
+                        }
+                    }
+
+                    if (!foundConductor)
+                        cir.setReturnValue(InteractionResult.FAIL);
+
+                    stackTag.put("SelectedPos", NbtUtils.writeBlockPos(pos));
+                    stackTag.remove("Bezier");
+                    itemInHand.setTag(stackTag);
+
+                    if (Config.CONDUCTOR_WHISTLE_REQUIRES_OWNING.get() && train.runtime.getSchedule() != null && !train.runtime.completed && !train.runtime.isAutoSchedule && train.getOwner(level) != pPlayer) {
+                        stackTag.remove("SelectedPos");
+                        itemInHand.setTag(stackTag);
+                        cir.setReturnValue(InteractionResult.FAIL);
+                    }
+
+
+
+                    if (train.runtime.getSchedule() != null && !train.runtime.isAutoSchedule) {
+                        ItemStack scheduleStack = train.runtime.returnSchedule();
+                        if (!scheduleStack.isEmpty()) {
+                            for (CompoundTag passengerTag : ((AccessorCarriage) conductorCarriage).getSerialisedPassengers().values()) {
+                                if (passengerTag.contains("PlayerPassenger")) continue;
+                                if (passengerTag.contains("id") && CREntities.CONDUCTOR.getId().equals(new ResourceLocation(passengerTag.getString("id")))) {
+                                    // It is a conductor
+                                    if (passengerTag.hasUUID("UUID") && passengerTag.getUUID("UUID").equals(conductorId)) {
+                                        // It is the targeted conductor
+                                        // Place the schedule in the conductor
+                                        ListTag schedulesList;
+                                        if (!passengerTag.contains("heldSchedules")) {
+                                            schedulesList = new ListTag();
+                                            passengerTag.put("heldSchedules", schedulesList);
+                                        } else {
+                                            schedulesList = passengerTag.getList("heldSchedules", Tag.TAG_COMPOUND);
+                                        }
+                                        schedulesList.add(scheduleStack.save(new CompoundTag()));
+                                        scheduleStack.setCount(0);
+                                        break;
+                                    }
+                                }
+                            }
+                            if (!scheduleStack.isEmpty()) {
+                                // Try if the conductor is simply loaded
+                                conductorCarriage.forEachPresentEntity(cce -> {
+                                    if (!scheduleStack.isEmpty()) {
+                                        for (Entity passenger : cce.getPassengers()) {
+                                            if (passenger instanceof ConductorEntity conductorEntity && passenger.getUUID().equals(conductorId)) {
+                                                conductorEntity.addSchedule(scheduleStack);
+                                                scheduleStack.setCount(0);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                            if (!scheduleStack.isEmpty() && !pPlayer.addItem(scheduleStack)) // fallback, probably should never be called. Keep it *just in case*
+                                pPlayer.drop(scheduleStack, false);
+                        }
+                    }
+
+
+                    Schedule schedule = new Schedule();
+                    ScheduleEntry entry = new ScheduleEntry();
+                    DestinationInstruction instruction = new DestinationInstruction();
+                    ScheduledDelay condition = new ScheduledDelay();
+                    condition.getData().putInt("Value", 0);
+                    instruction.getData().putString("Text", station.name);
+                    entry.instruction = instruction;
+                    if (entry.conditions.size() == 0)
+                        entry.conditions.add(new ArrayList<>());
+                    entry.conditions.get(0).add(condition);
+                    schedule.entries.add(entry);
+                    schedule.cyclic = false;
+                    train.runtime.setSchedule(schedule, true);
+                    ((AccessorScheduleRuntime) train.runtime).setCooldown(10);
+                    cir.setReturnValue(InteractionResult.SUCCESS);
+                }
+                else if(station != null && station.getPresentTrain() != null) {
+                    UUID trainId = station.getPresentTrain().id;
+                    Train train = Create.RAILWAYS.trains.get(trainId);
+                    if(train==null)
+                        cir.setReturnValue(InteractionResult.FAIL);
+
+                    AtomicBoolean found = new AtomicBoolean(false);
+                    for (Carriage carriage : train.carriages)
+                        carriage.forEachPresentEntity(e -> e.getIndirectPassengers()
+                                .forEach(p -> {
+                                    if (p instanceof ConductorEntity conductor && !found.get()) {
+                                        CompoundTag stackTag = itemInHand.getOrCreateTag();
+                                        stackTag.putUUID("SelectedTrain", train.id);
+                                        stackTag.putUUID("SelectedConductor", conductor.getUUID());
+                                        stackTag.putByte("SelectedColor", conductor.getEntityData().get(ConductorEntity.COLOR));
+                                        itemInHand.setTag(stackTag);
+                                        pPlayer.setItemInHand(pHand, itemInHand);
+                                        cir.setReturnValue(InteractionResult.SUCCESS);
+                                        found.set(true);
+                                    }
+                                }));
+                }
+            }
+        }
+    }
+
     @Inject(method = "use", at = @At(value = "RETURN", ordinal = 1), cancellable = true, remap = true)
     private void deployersAssemble(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit, CallbackInfoReturnable<InteractionResult> cir) {
         if (!pLevel.isClientSide && pPlayer instanceof DeployerFakePlayer deployerFakePlayer && pLevel.getBlockEntity(pPos) instanceof StationBlockEntity stationBe) {
