cbf0ce88528dd7ec7c1bded734d8972507438811
IThundxr <contact@ithundxr.dev>
2023-07-13 10:08:14 -0400

    :sob: rework code

--- common/src/main/java/com/railwayteam/railways/content/buffer/TrackBuffer.java
+++ common/src/main/java/com/railwayteam/railways/content/buffer/TrackBuffer.java
@@ -4,8 +4,8 @@ import com.simibubi.create.content.trains.graph.TrackNode;
 import com.simibubi.create.content.trains.signal.SingleBlockEntityEdgePoint;
 
 public class TrackBuffer extends SingleBlockEntityEdgePoint {
-    @Override
-    public boolean canNavigateVia(TrackNode side) {
-        return false;
-    }
+//    @Override
+//    public boolean canNavigateVia(TrackNode side) {
+//        return false;
+//    }
 }
--- common/src/main/java/com/railwayteam/railways/content/buffer/TrackBufferBlock.java
+++ common/src/main/java/com/railwayteam/railways/content/buffer/TrackBufferBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
