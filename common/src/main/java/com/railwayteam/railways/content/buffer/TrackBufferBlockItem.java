0787cab215fdd1671e59ae343986feab5e989928
IThundxr <contact@ithundxr.dev>
2023-07-16 09:47:21 -0400

    THE FINAL BRAINCELLLLL

--- common/src/main/java/com/railwayteam/railways/content/buffer/TrackBufferBlockItem.java
+++ common/src/main/java/com/railwayteam/railways/content/buffer/TrackBufferBlockItem.java
@@ -1,11 +1,17 @@
 package com.railwayteam.railways.content.buffer;
 
 
+import com.simibubi.create.AllSoundEvents;
 import com.simibubi.create.content.trains.graph.EdgePointType;
 import com.simibubi.create.content.trains.track.TrackBlockOutline;
 import com.simibubi.create.content.trains.track.TrackTargetingBlockItem;
+import com.simibubi.create.foundation.utility.Lang;
 import com.tterrag.registrate.util.nullness.NonNullBiFunction;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 
 public class TrackBufferBlockItem extends TrackTargetingBlockItem {
@@ -21,6 +27,17 @@ public class TrackBufferBlockItem extends TrackTargetingBlockItem {
 
     @Override
     public boolean useOnCurve(TrackBlockOutline.BezierPointSelection selection, ItemStack stack) {
+        Minecraft mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+        Level level = mc.level;
+
+        if (player != null) {
+
+            player.displayClientMessage(Lang.translateDirect("track_target.invalid")
+                    .withStyle(ChatFormatting.RED), true);
+            AllSoundEvents.DENY.play(level, player, player.position(), .5f, 1);
+            return false;
+        }
         return false;
     }
 }
