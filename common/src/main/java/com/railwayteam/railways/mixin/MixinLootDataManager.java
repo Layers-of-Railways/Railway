23cd92eda8c17b9262b1767806aa54d023ed6a05
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-07-22 21:54:01 -0700

    shut up loot data manager

---
+++ common/src/main/java/com/railwayteam/railways/mixin/MixinLootDataManager.java
@@ -0,0 +1,28 @@
+package com.railwayteam.railways.mixin;
+
+import com.google.gson.JsonElement;
+import com.railwayteam.railways.compat.tracks.TrackCompatUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.level.storage.loot.LootDataManager;
+import net.minecraft.world.level.storage.loot.LootDataType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(LootDataManager.class)
+public class MixinLootDataManager {
+    @Inject(method = {
+        "lambda$scheduleElementParse$4",
+//        "method_51195"
+    }, at = @At("HEAD"), cancellable = true)
+    // (LootDataType, ResourceManager, ResourceLocation, JsonElement, CallbackInfo;)V
+    private static void skipMissingLoot(LootDataType lootDataType, ResourceManager resourceManager, Map map, ResourceLocation id, JsonElement json, CallbackInfo ci) {
+        if (TrackCompatUtils.mixinIgnoreErrorForMissingItem(id)) {
+            ci.cancel();
+        }
+    }
+}
