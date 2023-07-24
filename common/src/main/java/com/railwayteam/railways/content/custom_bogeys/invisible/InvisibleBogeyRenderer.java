3d2bec1695ff523a014d5429e21bee8c444e4c10
IThundxr <contact@ithundxr.dev>
2023-07-24 15:49:22 -0400

    0.5.1c Update (#184)

--- common/src/main/java/com/railwayteam/railways/content/custom_bogeys/invisible/InvisibleBogeyRenderer.java
+++ common/src/main/java/com/railwayteam/railways/content/custom_bogeys/invisible/InvisibleBogeyRenderer.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.simibubi.create.content.trains.bogey.BogeyRenderer;
 import com.simibubi.create.content.trains.bogey.BogeySizes;
+import com.simibubi.create.content.trains.entity.CarriageBogey;
 import net.minecraft.nbt.CompoundTag;
 
 public class InvisibleBogeyRenderer extends BogeyRenderer {
@@ -19,7 +20,7 @@ public class InvisibleBogeyRenderer extends BogeyRenderer {
     }
 
     @Override
-    public void initialiseContraptionModelData(MaterialManager materialManager) {
+    public void initialiseContraptionModelData(MaterialManager materialManager, CarriageBogey carriageBogey) {
 
     }
 }
