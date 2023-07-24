f7f09a3d80051a54feca4001ff62fa1ac998b5a4
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-07-24 08:36:06 -0700

    fix: resize some textures to enable full mipping

---
+++ fabric/src/main/java/com/railwayteam/railways/fabric/mixin/PortingLibModelsMixin.java
@@ -0,0 +1,19 @@
+package com.railwayteam.railways.fabric.mixin;
+
+import com.railwayteam.railways.util.Utils;
+import io.github.fabricators_of_create.porting_lib.models.PortingLibModels;
+import io.github.fabricators_of_create.porting_lib.models.geometry.GeometryLoaderManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// fixme remove this mixin when porting lib gets fixed
+@Mixin(PortingLibModels.class)
+public class PortingLibModelsMixin {
+    @Inject(method = "onInitializeClient", at = @At("RETURN"), remap = false)
+    private void registerOnInitializeClient(CallbackInfo ci) {
+        if (Utils.isDevEnv())
+            GeometryLoaderManager.init();
+    }
+}
