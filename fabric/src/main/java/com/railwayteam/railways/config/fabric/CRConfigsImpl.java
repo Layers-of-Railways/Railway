a637b696a1ffb920a8f1838322bff22b669c75bd
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-08-14 17:36:52 -0700

    porty mc port

--- fabric/src/main/java/com/railwayteam/railways/config/fabric/CRConfigsImpl.java
+++ fabric/src/main/java/com/railwayteam/railways/config/fabric/CRConfigsImpl.java
@@ -3,8 +3,8 @@ package com.railwayteam.railways.config.fabric;
 import com.railwayteam.railways.Railways;
 import com.railwayteam.railways.config.CRConfigs;
 import com.simibubi.create.foundation.config.ConfigBase;
-import net.minecraftforge.api.ModLoadingContext;
-import net.minecraftforge.api.fml.event.config.ModConfigEvents;
+import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
+import fuzs.forgeconfigapiport.api.config.v2.ModConfigEvents;
 import net.minecraftforge.fml.config.ModConfig;
 
 import java.util.Map;
@@ -14,7 +14,7 @@ public class CRConfigsImpl {
         CRConfigs.registerCommon();
 
         for (Map.Entry<ModConfig.Type, ConfigBase> pair : CRConfigs.CONFIGS.entrySet())
-            ModLoadingContext.registerConfig(Railways.MODID, pair.getKey(), pair.getValue().specification);
+            ForgeConfigRegistry.INSTANCE.register(Railways.MODID, pair.getKey(), pair.getValue().specification);
 
         ModConfigEvents.loading(Railways.MODID).register(CRConfigs::onLoad);
         ModConfigEvents.reloading(Railways.MODID).register(CRConfigs::onReload);
