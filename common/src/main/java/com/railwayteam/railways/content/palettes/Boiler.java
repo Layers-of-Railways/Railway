a8f94366b8fce41b65ebf8e28c1a852cada88770
IThundxr <contact@ithundxr.dev>
2023-10-18 15:40:22 -0400

    Boiler Style stuff

--- common/src/main/java/com/railwayteam/railways/content/palettes/Boiler.java
+++ common/src/main/java/com/railwayteam/railways/content/palettes/Boiler.java
@@ -5,8 +5,11 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 
 public class Boiler extends Block implements IWrenchable {
+    //public static final EnumProperty<Style> STYLE = EnumProperty.create("style", Style.class)
+
     public Boiler(Properties properties) {
         super(properties);
     }
@@ -14,5 +17,9 @@ public class Boiler extends Block implements IWrenchable {
     @Override
     public InteractionResult onWrenched(BlockState state, UseOnContext context) {
         return IWrenchable.super.onWrenched(state, context);
+    }
+
+    enum Style {
+        GULLET, SMOKEBOX,
     }
 }
