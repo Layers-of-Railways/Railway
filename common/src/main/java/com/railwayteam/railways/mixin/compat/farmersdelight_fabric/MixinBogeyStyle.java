b7aa5deb6d94629f2479f5ca1806728de3fe7ec4
IThundxr <contact@ithundxr.dev>
2023-11-09 16:16:17 -0500

    eye candy in mixin javadocs

--- common/src/main/java/com/railwayteam/railways/mixin/compat/farmersdelight_fabric/MixinBogeyStyle.java
+++ common/src/main/java/com/railwayteam/railways/mixin/compat/farmersdelight_fabric/MixinBogeyStyle.java
@@ -2,26 +2,21 @@ package com.railwayteam.railways.mixin.compat.farmersdelight_fabric;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.simibubi.create.content.trains.bogey.BogeySizes;
+import com.railwayteam.railways.mixin.CRMixinPlugin;
 import com.simibubi.create.content.trains.bogey.BogeyStyle;
 import net.minecraft.core.DefaultedRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Map;
-
-/*
+/**
  * Hacky fix since the farmers delight fabric dev doesn't want to fix it on their side.
- * Checks if Farmers delight is loaded inside of com.railwayteam.railways.mixin.CRMixinPlugin#shouldApplyMixin
+ * Checks if Farmers delight is loaded inside of {@link CRMixinPlugin#shouldApplyMixin}
  */
 @Mixin(BogeyStyle.class)
 public class MixinBogeyStyle {
-    @Shadow private Map<BogeySizes.BogeySize, ResourceLocation> sizes;
-
     /*
      the DefaultedRegistry.get is a generic method, and thus returns an Object, with a CHECKCAST in bytecode
      ```
