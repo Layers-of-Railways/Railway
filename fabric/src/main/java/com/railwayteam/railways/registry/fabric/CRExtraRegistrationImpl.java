d24a0a80a58afde47ae48720fc3d9a93f8bbffaf
IThundxr <contact@ithundxr.dev>
2023-12-20 17:46:47 -0500

    porty port port

--- fabric/src/main/java/com/railwayteam/railways/registry/fabric/CRExtraRegistrationImpl.java
+++ fabric/src/main/java/com/railwayteam/railways/registry/fabric/CRExtraRegistrationImpl.java
@@ -3,14 +3,14 @@ package com.railwayteam.railways.registry.fabric;
 import com.railwayteam.railways.registry.CRExtraRegistration;
 import com.simibubi.create.Create;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 
 public class CRExtraRegistrationImpl {
     private static final ResourceLocation COPYCAT_ID = Create.asResource("copycat");
 
     public static void platformSpecificRegistration() {
-        RegistryEntryAddedCallback.event(Registry.BLOCK_ENTITY_TYPE).register((rawId, id, blockEntityType) ->  {
+        RegistryEntryAddedCallback.event(BuiltInRegistries.BLOCK_ENTITY_TYPE).register((rawId, id, blockEntityType) ->  {
             if (id == COPYCAT_ID)
                 CRExtraRegistration.addVentAsCopycat(blockEntityType);
         });
