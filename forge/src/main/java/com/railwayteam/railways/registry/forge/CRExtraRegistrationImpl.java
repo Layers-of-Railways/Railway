f60eec9026f3162b7836d77c5e3390302eee6892
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-12-26 17:06:42 -0800

    fix: forge CRExtraRegistrationImpl

--- forge/src/main/java/com/railwayteam/railways/registry/forge/CRExtraRegistrationImpl.java
+++ forge/src/main/java/com/railwayteam/railways/registry/forge/CRExtraRegistrationImpl.java
@@ -2,10 +2,10 @@ package com.railwayteam.railways.registry.forge;
 
 import com.railwayteam.railways.registry.CRExtraRegistration;
 import com.simibubi.create.Create;
-import net.minecraft.core.registries.Registries;
+import net.minecraft.core.Registry;
 
 public class CRExtraRegistrationImpl {
     public static void platformSpecificRegistration() {
-        Create.REGISTRATE.addRegisterCallback("copycat", Registries.BLOCK_ENTITY_TYPE, CRExtraRegistration::addVentAsCopycat);
+        Create.REGISTRATE.addRegisterCallback("copycat", Registry.BLOCK_ENTITY_TYPE_REGISTRY, CRExtraRegistration::addVentAsCopycat);
     }
 }
