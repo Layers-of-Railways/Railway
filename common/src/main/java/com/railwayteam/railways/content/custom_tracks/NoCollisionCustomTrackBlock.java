42edfbc1967d36f53143518a416d0cbbc283aa40
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-12-26 17:55:52 -0800

    fix: tieless and phantom track collisions

--- common/src/main/java/com/railwayteam/railways/content/custom_tracks/NoCollisionCustomTrackBlock.java
+++ common/src/main/java/com/railwayteam/railways/content/custom_tracks/NoCollisionCustomTrackBlock.java
@@ -1,6 +1,9 @@
 package com.railwayteam.railways.content.custom_tracks;
 
+import com.railwayteam.railways.content.custom_tracks.casing.CasingCollisionUtils;
+import com.railwayteam.railways.registry.CRShapes;
 import com.simibubi.create.content.trains.track.TrackBlock;
+import com.simibubi.create.content.trains.track.TrackBlockEntity;
 import com.simibubi.create.content.trains.track.TrackMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
@@ -11,11 +14,16 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class NoCollisionCustomTrackBlock extends TrackBlock {
     public NoCollisionCustomTrackBlock(Properties properties, TrackMaterial material) {
-        super(properties.noCollission(), material);
+        super(properties, material);
     }
 
     @Override
     public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        if (pLevel.getBlockEntity(pPos) instanceof TrackBlockEntity tbe) {
+            if (CasingCollisionUtils.shouldMakeCollision(tbe, pState)) {
+                return CRShapes.BOTTOM_SLAB;
+            }
+        }
         return Shapes.empty();
     }
 }
