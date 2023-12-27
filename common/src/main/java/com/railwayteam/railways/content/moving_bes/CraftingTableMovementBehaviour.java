bde8f88b61979d6773e4f5c014006c2ed63563f9
IThundxr <contact@ithundxr.dev>
2023-12-27 10:27:55 -0500

    Fix error making every block a crafting table when on trains, whoopsie

--- common/src/main/java/com/railwayteam/railways/content/moving_bes/CraftingTableMovementBehaviour.java
+++ common/src/main/java/com/railwayteam/railways/content/moving_bes/CraftingTableMovementBehaviour.java
@@ -1,38 +1,30 @@
-package com.railwayteam.railways.mixin;
+package com.railwayteam.railways.content.moving_bes;
 
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
 import com.simibubi.create.content.contraptions.Contraption;
-import com.simibubi.create.content.contraptions.MountedStorageManager;
+import com.simibubi.create.content.contraptions.behaviour.MovingInteractionBehaviour;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.CraftingMenu;
-import net.minecraft.world.level.block.CraftingTableBlock;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.phys.Vec3;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MountedStorageManager.class)
-public class MixinMountedStorageManager {
-    @Inject(method = "handlePlayerStorageInteraction", at = @At("HEAD"), cancellable = true)
-    private void handlePlayerStorageInteraction(Contraption contraption, Player player, BlockPos localPos, CallbackInfoReturnable<Boolean> cir) {
+public class CraftingTableMovementBehaviour extends MovingInteractionBehaviour {
+    @Override
+    public boolean handlePlayerInteraction(Player player, InteractionHand activeHand, BlockPos localPos, AbstractContraptionEntity contraptionEntity) {
+        Contraption contraption = contraptionEntity.getContraption();
         StructureTemplate.StructureBlockInfo info = contraption.getBlocks().get(localPos);
-
-        if (player.level.isClientSide()) {
-            if (info.state.getBlock() instanceof CraftingTableBlock)
-                cir.setReturnValue(true);
-        }
 
         Component name = info != null ? info.state.getBlock().getName() : Component.translatable("container.crafting");
         player.openMenu(new SimpleMenuProvider((syncId, inventory, player1) -> new CraftingMenu(syncId, inventory), name));
 
         Vec3 soundPos = contraption.entity.toGlobalVector(Vec3.atCenterOf(localPos), 0);
         player.level.playSound(null, new BlockPos(soundPos), SoundEvents.BARREL_OPEN, SoundSource.BLOCKS, 0.75f, 1f);
-        cir.setReturnValue(true);
+        return true;
     }
 }
