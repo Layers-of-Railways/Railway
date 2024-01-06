b9e3e191c36efd5c9c0ab35368a6e0e260b779b1
IThundxr <contact@ithundxr.dev>
2024-01-06 10:12:46 -0500

    i spy a bogey

---
+++ common/src/main/java/com/railwayteam/railways/api/Gauge.java
@@ -0,0 +1,8 @@
+package com.railwayteam.railways.api;
+
+/**
+ * Specifies what gauge a Bogey is for, Narrow and Wide are only used when Steam 'n' Rails is installed.
+ */
+public enum Gauge {
+    NARROW, STANDARD, WIDE
+}
---
+++ common/src/main/java/com/railwayteam/railways/impl/BogeyMenuManagerImpl.java
@@ -0,0 +1,18 @@
+package com.railwayteam.railways.impl;
+
+import com.railwayteam.railways.api.BogeyMenuManager;
+import com.simibubi.create.content.trains.bogey.BogeyStyle;
+import net.minecraft.resources.ResourceLocation;
+
+public class BogeyMenuManagerImpl implements BogeyMenuManager {
+
+    @Override
+    public void registerCategory(ResourceLocation id, ResourceLocation iconLocation) {
+
+    }
+
+    @Override
+    public void addToCategory(ResourceLocation categoryId, BogeyStyle bogeyStyle) {
+
+    }
+}
