a406d107fa129fe6c9c34e0e61155c5e8e89fc8b
IThundxr <me@ithundxr.dev>
2024-03-22 21:51:26 -0400

    Tweaknocompileonlydeperoo

--- common/src/main/java/com/railwayteam/railways/mixin/compat/malilib/MixinGuiTextFieldGeneric.java
+++ common/src/main/java/com/railwayteam/railways/mixin/compat/malilib/MixinGuiTextFieldGeneric.java
@@ -2,17 +2,18 @@ package com.railwayteam.railways.mixin.compat.malilib;
 
 import com.railwayteam.railways.annotation.ConditionalMixin;
 import com.railwayteam.railways.compat.Mods;
-import fi.dy.masa.malilib.gui.GuiTextFieldGeneric;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @ConditionalMixin(mods = Mods.MALILIB)
-@Mixin(GuiTextFieldGeneric.class)
+@Pseudo
+@Mixin(targets = "fi.dy.masa.malilib.gui.GuiTextFieldGeneric")
 public abstract class MixinGuiTextFieldGeneric extends EditBox {
     private MixinGuiTextFieldGeneric(Font font, int x, int y, int width, int height, Component message) {
         super(font, x, y, width, height, message);
