7cde7e662835756b0e24c9dc05a5f577fee66e6b
IThundxr <me@ithundxr.dev>
2024-03-30 13:32:09 -0400

    Choo Choo! You will arrive at your destination in ...
    - Refactor Fluid Fuel handler mixin's to use a common class and 2 impl's instead of 2 separate mixins
    - Make collisions with handcar's award train crash advancements

--- forge/src/main/java/com/railwayteam/railways/forge/mixin/TrainMixin.java
+++ forge/src/main/java/com/railwayteam/railways/forge/mixin/TrainMixin.java
@@ -1,78 +1,21 @@
 package com.railwayteam.railways.forge.mixin;
 
-import com.railwayteam.railways.content.fuel.LiquidFuelManager;
-import com.railwayteam.railways.content.fuel.LiquidFuelType;
-import com.railwayteam.railways.mixin_interfaces.IFuelInventory;
 import com.railwayteam.railways.registry.CRTags;
-import com.simibubi.create.content.trains.entity.Carriage;
 import com.simibubi.create.content.trains.entity.Train;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraftforge.common.ForgeHooks;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.IFluidHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
-@Mixin(value = Train.class, remap = false)
+@Mixin(Train.class)
 public class TrainMixin {
-    @Shadow public double speed;
-    @Shadow public List<Carriage> carriages;
-    @Shadow public int fuelTicks;
-
-    @ModifyArg(method = "burnFuel", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/ForgeHooks;getBurnTime(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/crafting/RecipeType;)I"))
+    @ModifyArg(method = "burnFuel", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/ForgeHooks;getBurnTime(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/crafting/RecipeType;)I"), remap = false)
     private ItemStack railways$disableFuelConsumptionBasedOnTag(ItemStack stack) {
         if (stack.is(CRTags.AllItemTags.NOT_TRAIN_FUEL.tag)) {
             return Items.AIR.getDefaultInstance();
         }
         return stack;
-    }
-
-    @Inject(method = "burnFuel", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I", shift = At.Shift.AFTER))
-    private void railways$burnFuel(CallbackInfo ci) {
-        boolean iterateFromBack = speed < 0;
-        int carriageCount = carriages.size();
-
-        for (int index = 0; index < carriageCount; index++) {
-            int i = iterateFromBack ? carriageCount - 1 - index : index;
-            Carriage carriage = carriages.get(i);
-            IFluidHandler fuelFluids = ((IFuelInventory) carriage.storage).railways$getFuelFluids();
-            if (fuelFluids == null)
-                continue;
-
-            for (int tanks = 0; tanks < fuelFluids.getTanks(); tanks++) {
-                FluidStack fluidStack = fuelFluids.drain(100, IFluidHandler.FluidAction.SIMULATE);
-
-                int burnTime;
-                Fluid fluid = fluidStack.getFluid();
-                LiquidFuelType fuelType = LiquidFuelManager.getTypeForFluid(fluid);
-                if (fuelType != null) {
-                    burnTime = fuelType.getFuelTicks();
-                } else {
-                    int bucketBurnTime = ForgeHooks.getBurnTime(fluid.getBucket().getDefaultInstance(), null);
-
-                    // Divide burnTime by 100 to get burnTime for 1/10th of a bucket and then by divide by 4,
-                    // so it isn't so strong
-                    burnTime = (bucketBurnTime / 100) / 4;
-                }
-
-                if (burnTime <= 0)
-                    continue;
-
-                // Extract 100 Mb worth of fluid (1/10th of a bucket)
-                fuelFluids.drain(100, IFluidHandler.FluidAction.EXECUTE);
-
-                fuelTicks += burnTime;
-                return;
-            }
-        }
     }
 }
 
