d283f04c7fe78822cd99cbce27386ec3c02d9d29
IThundxr <me@ithundxr.dev>
2024-03-30 14:03:31 -0400

    Casting away
    - Suppress casting warning

--- fabric/src/main/java/com/railwayteam/railways/fabric/mixin/TrainMixin.java
+++ fabric/src/main/java/com/railwayteam/railways/fabric/mixin/TrainMixin.java
@@ -13,9 +13,10 @@ import java.util.Iterator;
 
 @Mixin(Train.class)
 public class TrainMixin {
+    @SuppressWarnings("unchecked")
     @ModifyExpressionValue(method = "burnFuel", at = @At(value = "INVOKE", target = "Lio/github/fabricators_of_create/porting_lib/transfer/TransferUtil;getNonEmpty(Lnet/fabricmc/fabric/api/transfer/v1/storage/Storage;)Ljava/lang/Iterable;"), remap = false)
     private <T> Iterable<? extends StorageView<T>> railways$disableFuelConsumptionBasedOnTag(Iterable<? extends StorageView<T>> original) {
-        return () -> (Iterator) Iterators.filter(original.iterator(), it ->
+        return () -> (Iterator<StorageView<T>>) Iterators.filter(original.iterator(), it ->
                 !((ItemVariant) it.getResource()).getItem().getDefaultInstance().is(CRTags.AllItemTags.NOT_TRAIN_FUEL.tag)
         );
     }
