75b31055304ac4d711d2687ddfcc8720cf6ab850
kurocat471 <74220178+kurocat471@users.noreply.github.com>
2024-03-31 23:10:10 -0500

    fixed the boiler placement helper direction

--- common/src/main/java/com/railwayteam/railways/content/boiler/BoilerBlock.java
+++ common/src/main/java/com/railwayteam/railways/content/boiler/BoilerBlock.java
@@ -270,6 +270,7 @@ public class BoilerBlock extends Block implements IWrenchable, IForceRenderingSo
 
             List<Direction> directions = IPlacementHelper.orderedByDistance(pos, ray.getLocation(), dir -> dir.getAxis() == axisFunction.apply(state));
             for (Direction dir : directions) {
+                dir = dir.getOpposite();
                 int range = AllConfigs.server().equipment.placementAssistRange.get();
                 if (player != null) {
                     AttributeInstance reach = player.getAttribute(getAttribute());
