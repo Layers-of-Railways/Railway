76ed5c8179a67234434f97a39105c5427be10999
IThundxr <me@ithundxr.dev>
2024-04-03 21:24:41 -0400

    update annotation

--- common/src/main/java/com/railwayteam/railways/annotation/event/MultiLoaderEvent.java
+++ common/src/main/java/com/railwayteam/railways/annotation/event/MultiLoaderEvent.java
@@ -1,12 +1,15 @@
 package com.railwayteam.railways.annotation.event;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Used to mark methods that are called via abstracted loader-specific events.
  * <p>
- * Meant purely for documentation.
+ * Meant purely for documentation and does not exist at compile-time or runtime.
  */
 @Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
 public @interface MultiLoaderEvent { }
