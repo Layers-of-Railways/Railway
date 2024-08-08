5b5c429440548078a9a32b9ff46a51f1cc2b48f0
techno-sam <77073745+techno-sam@users.noreply.github.com>
2024-08-08 18:06:53 +0200

    Revert "fix dev env mixin"
    This reverts commit 8819065b351cf0d82e098bfdc5dcc8454b925158.

--- common/src/main/java/com/railwayteam/railways/annotation/mixin/DevEnvMixin.java
+++ common/src/main/java/com/railwayteam/railways/annotation/mixin/DevEnvMixin.java
@@ -26,6 +26,6 @@ import java.lang.annotation.Target;
 /**
  * You bear all the consequences of using this. Don't shoot yourself in the foot I guess? Try wearing proper mixin equipment?
  */
-@Target({ElementType.TYPE})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DevEnvMixin {}
