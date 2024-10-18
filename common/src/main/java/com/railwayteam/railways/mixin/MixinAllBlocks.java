cdb7b58ffed0f8e44f50dc7205b21d6b968df35a
IThundxr <me@ithundxr.dev>
2024-10-18 17:21:53 -0400

    chore: Switch to ModifyExpressionValue as WrapOp isn't needed here

--- common/src/main/java/com/railwayteam/railways/mixin/MixinAllBlocks.java
+++ common/src/main/java/com/railwayteam/railways/mixin/MixinAllBlocks.java
@@ -18,17 +18,14 @@
 
 package com.railwayteam.railways.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.railwayteam.railways.annotation.mixin.ConditionalMixin;
 import com.railwayteam.railways.compat.Mods;
 import com.railwayteam.railways.content.animated_flywheel.FlywheelMovementBehaviour;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllMovementBehaviours;
 import com.tterrag.registrate.builders.BlockBuilder;
-import com.tterrag.registrate.util.nullness.NonNullUnaryOperator;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Slice;
@@ -37,7 +34,7 @@ import org.spongepowered.asm.mixin.injection.Slice;
 @ConditionalMixin(mods = Mods.EXTENDEDFLYWHEELS, applyIfPresent = false)
 @Mixin(AllBlocks.class)
 public class MixinAllBlocks {
-    @WrapOperation(
+    @ModifyExpressionValue(
             method = "<clinit>",
             at = @At(
                 value = "INVOKE",
@@ -49,7 +46,7 @@ public class MixinAllBlocks {
             ),
             remap = false
     )
-    private static <T extends Block, P> BlockBuilder<T, P> railways$addFlywheelMovementBehaviour(BlockBuilder<T, P> instance, NonNullUnaryOperator<BlockBehaviour.Properties> func, Operation<BlockBuilder<T, P>> original) {
-        return original.call(instance, func).onRegister(AllMovementBehaviours.movementBehaviour(new FlywheelMovementBehaviour()));
+    private static <T extends Block, P> BlockBuilder<T, P> railways$addFlywheelMovementBehaviour(BlockBuilder<T, P> original) {
+        return original.onRegister(AllMovementBehaviours.movementBehaviour(new FlywheelMovementBehaviour()));
     }
 }
