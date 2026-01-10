cat4blep/Create-Steam-n-Rails-Fly

fix StationLoadedCondition status text

common/src/main/java/com/railwayteam/railways/content/schedule/StationLoadedCondition.java
@@ -68,6 +68,6 @@ public class StationLoadedCondition extends ScheduleWaitCondition {
 
     @Override
     public MutableComponent getWaitingStatus(Level level, Train train, CompoundTag tag) {
-        return Component.translatable("railways.schedule.condition.unloaded.status");
+        return Component.translatable("railways.schedule.condition.loaded.status");
     }
 }

Layers-of-Railways/Railway

common/src/main/java/com/railwayteam/railways/content/schedule/StationLoadedCondition.java
@@ -69,6 +69,6 @@ public class StationLoadedCondition extends ScheduleWaitCondition {
 
     @Override
     public MutableComponent getWaitingStatus(Level level, Train train, CompoundTag tag) {
-        return Components.translatable("railways.schedule.condition.unloaded.status");
+        return Components.translatable("railways.schedule.condition.loaded.status");
     }
 }