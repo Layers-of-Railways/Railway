4a5287d0e68c52fbfde2af07506f2d6376ad3ed5
techno-sam <77073745+techno-sam@users.noreply.github.com>
2026-02-22 11:46:58 +0100

    universal shadow wrench config

--- common/src/main/java/com/railwayteam/railways/config/CClient.java
+++ common/src/main/java/com/railwayteam/railways/config/CClient.java
@@ -36,6 +36,7 @@ public class CClient extends ConfigBase {
     public final ConfigBool useDevCape = b(true, "useDevCape", Comments.useDevCape, Comments.useDevCape2);
     public final ConfigBool renderNormalCap = b(true, "renderNormalCap", Comments.renderNormalCap);
     public final ConfigBool animatedFlywheels = b(true, "animatedFlywheels", Comments.animatedFlywheels);
+    public final ConfigBool universalShadowWrench = b(false, "universalShadowWrench", Comments.universalShadowWrench);
 
     // smoke
     public final ConfigGroup smoke = group(1, "smoke", Comments.smoke);
@@ -75,6 +76,7 @@ public class CClient extends ConfigBase {
         static String useDevCape2 = "This setting may require a relog to take effect";
         static String renderNormalCap = "Should the normal create conductor cap be rendered on top of the conductors existing hat?";
         static String animatedFlywheels = "Should flywheels and blocks extending the FlywheelBlock class be animated when apart of trains?";
+        static String universalShadowWrench = "Allow all wrenches to auto-fill `/snr shadow_realm banish` when sneak-used, without requiring the tag ShadowHammer.";
 
         static String smoke = "Smoke Settings";
         static String oldSmoke = "Old-style Smoke Settings";
