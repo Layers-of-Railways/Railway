d6c8f29d9daa9a5812350eab4b12b833070d8f58
techno-sam <77073745+techno-sam@users.noreply.github.com>
2026-03-01 22:12:31 +0100

    fix typo

--- common/src/main/java/com/railwayteam/railways/registry/commands/ShadowRealmCommand.java
+++ common/src/main/java/com/railwayteam/railways/registry/commands/ShadowRealmCommand.java
@@ -115,7 +115,7 @@ public class ShadowRealmCommand {
 
         CRPackets.PACKETS.sendTo(player, new ShadowTrainRestorePacket(train));
 
-        source.sendSuccess(() -> Components.literal("Use a wrench on a a track to restore '").append(train.name).append("'"), true);
+        source.sendSuccess(() -> Components.literal("Use a wrench on a track to restore '").append(train.name).append("'"), true);
         return 1;
     }
     
