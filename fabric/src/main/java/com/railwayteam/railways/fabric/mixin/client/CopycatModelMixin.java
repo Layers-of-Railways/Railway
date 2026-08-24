9fdd56ee686c99df93cd53679ab57bd1dc3780b8
Yokuyin <patrick.hooijer@gmail.com>
2026-08-24 21:30:56 +0200

    Fix #765: Connecting a Copycat Panel with AE2 Cable Facade causes client-side crash

--- fabric/src/main/java/com/railwayteam/railways/fabric/mixin/client/CopycatModelMixin.java
+++ fabric/src/main/java/com/railwayteam/railways/fabric/mixin/client/CopycatModelMixin.java
@@ -20,10 +20,12 @@ package com.railwayteam.railways.fabric.mixin.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.railwayteam.railways.fabric.FilteredRenderAttachedBlockView;
 import com.simibubi.create.content.decoration.copycat.CopycatBlock;
 import com.simibubi.create.content.decoration.copycat.CopycatModel;
 import com.simibubi.create.content.decoration.copycat.FilteredBlockAndTintGetter;
 import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
+import net.fabricmc.fabric.api.rendering.data.v1.RenderAttachedBlockView;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -32,8 +34,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
 
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+@SuppressWarnings("deprecation")
 @Mixin(CopycatModel.class)
 public class CopycatModelMixin {
     @WrapOperation(method = "emitBlockQuads", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/decoration/copycat/CopycatModel;emitBlockQuadsInner(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Ljava/util/function/Supplier;Lnet/fabricmc/fabric/api/renderer/v1/render/RenderContext;Lnet/minecraft/world/level/block/state/BlockState;Lcom/simibubi/create/content/decoration/copycat/CopycatModel$CullFaceRemovalData;Lcom/simibubi/create/content/decoration/copycat/CopycatModel$OcclusionData;)V"))
@@ -43,7 +47,12 @@ public class CopycatModelMixin {
                                     @Coerce Object occlusionData, Operation<Void> original) {
         BlockAndTintGetter filteredView;
         if (state.getBlock() instanceof CopycatBlock copycatBlock) {
-            filteredView = new FilteredBlockAndTintGetter(blockView, targetPos -> copycatBlock.canConnectTexturesToward(blockView, pos, targetPos, state));
+            Predicate<BlockPos> filter = targetPos -> copycatBlock.canConnectTexturesToward(blockView, pos, targetPos, state);
+            if (blockView instanceof RenderAttachedBlockView renderAttachedBlockView) {
+                filteredView = new FilteredRenderAttachedBlockView(renderAttachedBlockView, filter);
+            } else {
+                filteredView = new FilteredBlockAndTintGetter(blockView, filter);
+            }
         } else {
             filteredView = blockView;
         }
